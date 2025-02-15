e663ecc83a41ae51fe52c247c54e98d7cb26995a
anthonyhalim150 <anthonyhalim153@gmail.com>
2025-02-16 06:19:02 +0800

    Change local

--- app/backend/routes/itemRoutes.js
+++ app/backend/routes/itemRoutes.js
@@ -2,13 +2,6 @@ const express = require("express");
 const { getAllItems, getItem} = require("../controllers/itemController");
 
 const router = express.Router();
-const app = express();
-const cors = require("cors");
-
-app.use(cors({
-    origin: "https://localhost:3000",
-    credentials: true
-}));
 
 router.get("/redeem", getAllItems);
 router.get("/redeem-ones", getItem);
